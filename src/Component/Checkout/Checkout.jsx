1331c876cba6aef65a40cfcccd4e6e1011b351de
Remonda <Rue281@gmail.com>
2023-11-21 11:37:27 +0200

    refactoring checkout form validation

--- src/Component/Checkout/Checkout.jsx
+++ src/Component/Checkout/Checkout.jsx
@@ -1,28 +1,41 @@
 import axios from 'axios';
 import { useFormik } from 'formik'
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom';
+import * as Yup from 'yup'
 
 export default function Checkout() {
     /*TODO:
-        -form validation
+        -form validation //Done
         -loading bar before stripe forwarding 
     */
 
+    //let [errorMsg, setErrorMsg]=useState("");
+    let [loadingSpinner, setLoadingSpinner]=useState(true);
+
     let baseUrl = "https://route-ecommerce.onrender.com";
+    //let baseUrl = "https://ecommerce.routemisr.com"
+
     let {cartId} = useParams();
 
+    let validation = Yup.object({
+
+        AddressDetails:Yup.string().required("Address is a required field").matches(/^[a-zA-Z0-9\s,'-]*$/,"enter a valid address"),
+        phone:Yup.string().required().matches(/^01(2|1|0|5)[0-9]{8}$/,"enter valid phone number"),
+        city:Yup.string().required().matches(/^[Aa-zZ]*$/,"enter valid city")
+    });
 
     let formik = useFormik({
         initialValues:{
-            details: "",
+            AddressDetails: "",
             phone:"",
             city:""
         },
         onSubmit:(values)=>{
             console.log(values);
             checkout(values,cartId);
-        }
+        },
+        validationSchema:validation
     })
 
     async function checkout(vals,id){
@@ -32,9 +45,11 @@ export default function Checkout() {
         let headers = {
             token:localStorage.getItem("token")
         }
-        console.log(body)
+        console.log(body);
+        setLoadingSpinner(false);
         let {data} = await axios.post(`${baseUrl}/api/v1/orders/checkout-session/${id}?url=http://localhost:4200`,body,{headers}).catch(err=>console.log(err));
         console.log(data);
+        setLoadingSpinner(true);
         if(data.status == "success"){
             window.open(data.session.url,"_self")
         }
@@ -43,18 +58,28 @@ export default function Checkout() {
     <div>
         <form onSubmit={formik.handleSubmit}>
             <div className='my-2'>
-                <label htmlFor="details">Details</label>
-                <input onChange={formik.handleChange} type="text" name='details' id='details' className='form-control'/>
+                <label htmlFor="AddressDetails">Address</label>
+                <input onBlur={formik.handleBlur} onChange={formik.handleChange} type="text" name='AddressDetails' id='AddressDetails' className='form-control'/>
+                {formik.touched.AddressDetails && formik.errors.AddressDetails? 
+                <p className='text-danger'>{formik.errors.AddressDetails}</p> 
+                : ""}
             </div>
             <div className='my-2'>
                 <label htmlFor="phone">Phone</label>
-                <input onChange={formik.handleChange} type="text" name='phone' id='phone' className='form-control'/>
+                <input onBlur={formik.handleBlur} onChange={formik.handleChange} type="text" name='phone' id='phone' className='form-control'/>
+                {formik.touched.phone && formik.errors.phone? 
+                <p className='text-danger'>{formik.errors.phone}</p> 
+                : ""}
             </div>
             <div className='my-2'>
                 <label htmlFor="city">City</label>
-                <input onChange={formik.handleChange} type="text" name='city' id='city' className='form-control'/>
+                <input onBlur={formik.handleBlur} onChange={formik.handleChange} type="text" name='city' id='city' className='form-control'/>
+                {formik.touched.city && formik.errors.city? 
+                <p className='text-danger'>{formik.errors.city}</p> 
+                : ""}
             </div>
-            <button className='btn btn-success'>Pay</button>
+            {loadingSpinner ? <button type="submit" disabled={!formik.isValid}  className='btn btn-success'>Pay</button> : 
+            <button type="button" className='btn ms-2'> <i className="fa-solid fa-spinner fa-spin"></i></button>} 
         </form>
     </div>
   )
--- src/Component/Profile/Profile.jsx
+++ src/Component/Profile/Profile.jsx
@@ -1,10 +1,96 @@
-import React from 'react'
+import axios from 'axios';
+import { useFormik } from 'formik'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import * as Yup from 'yup'
 
 export default function Profile({userData}) {
+
+  /**TODO: add form to update user data */
+
+  let [errorMsg, setErrorMsg]=useState("");
+  let [loadingSpinner, setLoadingSpinner]=useState(true);
+
+  //let baseUrl = "https://route-ecommerce.onrender.com";
+  let baseUrl = "https://route-ecommerce-app.vercel.app";
+
+  //programming navigation
+  let navigate = useNavigate();
+
+  let validation = Yup.object({
+    name:Yup.string().required().min(2,"min chars is 2").max(7,"max chars is 10"),
+    email:Yup.string().required().email("enter valid email"),
+    phone:Yup.string().required().matches(/^01(2|1|0|5)[0-9]{8}$/,"enter valid phone number"),
+    password:Yup.string().required().matches(/^[A-Z][a-z0-9!@#$%^&*()_-]{6,16}$/,"enter valid password"),
+    rePassword:Yup.string().oneOf([Yup.ref("password")],"password is not matched")
+  });
+
+  let formik = useFormik({
+    initialValues:{
+      name:"",
+      email:"",
+      password:"",
+      rePassword:"",
+      phone:""
+    },
+    onSubmit:(values)=>{
+      console.log(values);
+      //addAddress(values);
+    },
+    validationSchema:validation
+  });
+
+  //Add address to user
+  async function addAddress(values){
+    // let response
+    setLoadingSpinner(false);
+    let {data} = await axios.post(`${baseUrl}/api/v1/addresses`,values).catch(error =>{setLoadingSpinner(true);
+      console.log(error);
+    console.log(error.response.data.errors.msg);
+    setErrorMsg(error.response.data.errors.msg)});
+    console.log(data);
+    if(data.message === "success"){
+      setLoadingSpinner(true);
+       
+      //save token
+      localStorage.setItem("token",data.token);
+      //navigate to Login
+      navigate("/login")
+    }
+  }
   
   return (
     <div>
       <h2>name:{userData?.name}</h2>
+
+      {/* <div>
+      <h1>Registration Form</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <div className='my-3'>
+          <label htmlFor="name">name</label>
+          <input onBlur={formik.handleBlur} onChange={formik.handleChange} type="text" name='name'id='name' className='form-control mt-2 bg-transparent text-white'/>
+          {formik.touched.name && formik.errors.name? 
+           <p className='text-danger'>{formik.errors.name}</p> 
+          : ""}
+          
+        </div>
+        <div className='my-3'>
+          <label htmlFor="phone">phone</label>
+          <input onBlur={formik.handleBlur} onChange={formik.handleChange} type="text" name='phone'id='phone' className='form-control mt-2 bg-transparent text-white'/>
+          {formik.touched.phone && formik.errors.phone? 
+           <p className='text-danger'>{formik.errors.phone}</p> 
+          : ""}
+        </div>
+        {errorMsg !== ""? <div className="alert alert-danger">{errorMsg}</div> : ""}
+        
+        {loadingSpinner ? <button type="submit" disabled={!formik.isValid} className='btn btn-info'>Register</button> :<button type="button" className='btn btn-info ms-2'>
+        <i className="fa-solid fa-spinner fa-spin"></i>
+        </button>}
+        
+        
+        
+      </form>
+    </div> */}
     </div>
   )
 }
